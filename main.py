a3caefd54ea3516401967dfa3980560041092a5f
Илья Бородин <Ilya.borodin.1986@yandex.ru>
2023-05-27 14:13:06 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ def attack(char_name, char_class):
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
     if char_class == 'mage':
-        return (f'{char_name} нанёс урон противнику равный'
+        return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику равный '
+        return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(-3, -1)}')
+    return ('Ты нажал что-то не то')
 
 
 def defence(char_name, char_class):
@@ -20,6 +21,7 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+    return ('Ты нажал что-то не то')
 
 
 def special(char_name, char_class):
@@ -30,6 +32,7 @@ def special(char_name, char_class):
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    return ('Ты нажал что-то не то')
 
 
 def start_training(char_name, char_class):
