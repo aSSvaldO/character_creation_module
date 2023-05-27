3fa3c0e6cced6c4ffed53be8827d60e870387799
Илья Бородин <Ilya.borodin.1986@yandex.ru>
2023-05-27 13:06:00 +0300

    второе исправление по flast8

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from random import randint
 
 def attack(c_name, char_class):
     if char_class == 'warrior':
-        return (f'{c_name} нанёс урон противнику равный {5 + randint(3, 5)}')
+        return (f'{c_name} нанёс урон противнику равный '
+                f'{5 + randint(3, 5)}')
     if char_class == 'mage':
-        return (f'{c_name} нанёс урон противнику равный {5 + randint(5, 10)}')
+        return (f'{c_name} нанёс урон противнику равный'
+                f'{5 + randint(5, 10)}')
     if char_class == 'healer':
-        return (f'{c_name} нанёс урон противнику равный {5 + randint(-3, -1)}')
+        return (f'{c_name} нанёс урон противнику равный '
+                f'{5 + randint(-3, -1)}')
 def defence(char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
@@ -17,7 +20,8 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
 def special(char_name, char_class):
     if char_class == 'warrior': 
-        return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
+        return (f'{char_name} применил специальное умение '
+                f' «Выносливость {80 + 25}»')
     if char_class == 'mage':
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
@@ -52,14 +56,20 @@ def choice_char_class():
     approve_choice = None
     c_class = None
     while approve_choice != 'y':
-        c_class = input('Введи название персонажа, за которого хочешь играть: Воитель — warrior, Маг — mage, Лекарь — healer: ')
+        c_class = input('Введи название персонажа, за которого '
+                        'хочешь играть: Воитель — warrior, '
+                        ' Маг — mage, Лекарь — healer: ')
         if c_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя. Сильный, выносливый и отважный.')
+            print('Воитель — дерзкий воин ближнего боя. '
+                  'Сильный, выносливый и отважный.')
         if c_class == 'mage':
-            print('Маг — находчивый воин дальнего боя. Обладает высоким интеллектом.')
+            print('Маг — находчивый воин дальнего боя. '
+                  'Обладает высоким интеллектом.')
         if c_class == 'healer':
-            print('Лекарь — могущественный заклинатель. Черпает силы из природы, веры и духов.')
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку, чтобы выбрать другого персонажа ').lower()
+            print('Лекарь — могущественный заклинатель. '
+                  'Черпает силы из природы, веры и духов.')
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или '
+                               'любую другую кнопку, чтобы выбрать другого персонажа ').lower()
     return c_class
 
 
